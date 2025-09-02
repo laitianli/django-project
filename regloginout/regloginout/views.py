909fab57cb89de8589896f371ee5261de30442fd
Tianli Lai <laitianli@tom.com>
2025-09-02 17:22:51 +0800

    app: show login page if not login at mainpage

--- regloginout/regloginout/views.py
+++ regloginout/regloginout/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+
+##通过request.user.is_authenticated变量判断用户是有用户登录
+# def logout_view(request):
+#     is_login = request.user.is_authenticated
+#     print(f'[user_login_logout] is_login: {is_login}')
+#     auth.logout(request)
+#     is_login = request.user.is_authenticated
+#     print(f'[user_login_logout] afte logout, is_login: {is_login}')
+#     return HttpResponse('用户退出登录！')
+
+##如何没有登录，则跳转到【登录页面】
+@login_required(login_url='/accounts/login/')
 def mainPage(request):
     host_with_port = request.get_host() 
     print(host_with_port)
